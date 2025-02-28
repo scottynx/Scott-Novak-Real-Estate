c840f11fa0d1022d803434f7bdc6687f0b72d0fd
Scott Novak <scottynx@users.noreply.github.com>
2025-02-28 08:30:29 -0800

    redirect from /terms to /smsterms

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/terms',
+        destination: '/smsterms',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
